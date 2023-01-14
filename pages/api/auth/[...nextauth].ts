5b4f6e4266bb53c32a9916a3b0513856dcd894d8
sikethedev <ivoinestrachan242@gmail.com>
2023-01-14 14:38:32 -0500

    added auth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID as string,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+    }),
+    // ...add more providers here
+  ],
+};
+
+export default NextAuth(authOptions);
