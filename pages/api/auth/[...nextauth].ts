5d5d7ef9ac040a36c57e6e9b4173feb12b3f3a4b
sikethedev <ivoinestrachan242@gmail.com>
2023-01-15 19:48:11 -0500

    added session management

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,7 +2,6 @@ import NextAuth from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
 export const authOptions = {
-  // Configure one or more authentication providers
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID as string,
@@ -10,6 +9,22 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user.id = user.id;
+      session.user.username = user.username as string;
+      return session;
+    },
+    async signIn({ user }) {
+      if (!user.email) {
+        return false;
+      }
+   
+   
+      return true;
+    },
+    
+  },
 };
 
 export default NextAuth(authOptions);
